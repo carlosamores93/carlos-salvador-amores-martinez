d3d5e1b5c8287f694a8f3689dc9ee0a93dc17e5f
Carlos <amorescarlos93@hotmail.com>
2019-05-27 20:12:11 +0200

    m,as

--- resources/views/frontend/bautizo_camila/work.blade.php
+++ resources/views/frontend/bautizo_camila/work.blade.php
@@ -24,7 +24,7 @@
                 <div class="row">
                   <div class="col-sm-12">
                     <h2 class="w-title">Parroquia Nuestra Señora de los Apóstoles</h2>
-                    <div class="w-more">
+                    <div class="w-more" style="text-align: justify;">
                       <span class="w-ctegory" style="color:black;"> Esta es la iglesia elegida para celebrar mi bautizo. La ceremonia empieza a las 12:30, y está ubicada en la <a href="https://www.google.com/maps/place/Parroquia+Nuestra+Se%C3%B1ora+de+los+Ap%C3%B3stoles/@40.4029285,-3.6373827,150m/data=!3m1!1e3!4m13!1m7!3m6!1s0xd4225ecd0c5bf63:0x2df9c554f8298330!2sMoratalaz,+28030+Madrid!3b1!8m2!3d40.4072203!4d-3.6570055!3m4!1s0xd422590f87dcaef:0x2a04e7098b73aa23!8m2!3d40.4028738!4d-3.6373879" target="_blank" style="color:blue;">calle Luis de Hoyos Sainz, 94B, 28030 Madrid (click aquí para ver en el mapa)</a></span>
                     </div>
                   </div>
@@ -42,7 +42,7 @@
                 <div class="row">
                   <div class="col-sm-12">
                     <h2 class="w-title">Bar Restaurante Mil Amores</h2>
-                    <div class="w-more">
+                    <div class="w-more" style="text-align: justify;">
                       <span class="w-ctegory"  style="color:black;"> Este es el sitio elegido para celebrar mi bautizo, está ubicado en la <a href="https://www.google.com/maps/place/Calle+Puerto+del+Escudo,+26,+28919+Legan%C3%A9s,+Madrid/@40.3503644,-3.728899,17z/data=!3m1!4b1!4m5!3m4!1s0xd422741c354e469:0x9ec25d732de86690!8m2!3d40.3503644!4d-3.7267103" target="_blank" style="color:blue;">calle Puerto del Escudo,26,Leganés, Madrid (click aquí para ver en el mapa)</a>. La hora de llegada se calcula que será sobre las 15:00 y la hora de servir la comida entre las 16:00 - 16:30.<br>Para los más golosos.. el menú se compone de 1º plato un <b>consomé</b> y de segundo plato <b><a href="img/bandera.jpg" target="_blank" style="color:blue;"> una bandera</a></b>🇪🇨 y de bebida se puede elegir entre <b>coca/cola, agua, vino y/o casera</b><br>Por cierto, tenemos el local hasta las 06:00 de la mañana 🍻 🍺 🥃 🍾 💃 🕺 </span>
                     </div>
                   </div>
@@ -60,9 +60,9 @@
                 <div class="row">
                   <div class="col-sm-12">
                     <h2 class="w-title">Regalos</h2>
-                    <div class="w-more">
+                    <div class="w-more" style="text-align: justify;">
                       <span class="w-ctegory"  style="color:black;">
-                        Los regalos🎁 puede ser lo que ustedes deseen, ropa, juguetes o si les resulta más comodo una lluvia de sobres📨 💶 💰.<br> En el lugar de recepción habrá un lugar donde podrán depositar sus regalos.
+                        Los regalos🎁 puede ser lo que ustedes deseen, ropa, juguetes o si les resulta más cómodo una lluvia de sobres📨 💶 💰.<br> En el lugar de recepción habrá un lugar donde podrán depositar sus regalos.
                       </span>
                     </div>
                   </div>
