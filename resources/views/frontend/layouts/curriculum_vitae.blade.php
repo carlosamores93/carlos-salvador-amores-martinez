4ac5aeaa560a4d8e74801e2182882178f410db47
Carlos <amorescarlos93@hotmail.com>
2019-06-12 23:47:33 +0200

    Cookiebot analitys and googletagmanager

--- resources/views/frontend/layouts/curriculum_vitae.blade.php
+++ resources/views/frontend/layouts/curriculum_vitae.blade.php
@@ -24,6 +24,18 @@
         <!-- Main Stylesheet File -->
         <link href="{{ asset('curriculum_vitae/css/style.css') }}" rel="stylesheet">
 
+        <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="44a44001-0eb9-4ff3-817e-f2118aee54ae" type="text/javascript" async></script>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script type="text/plain" data-cookieconsent="marketing" async src="https://www.googletagmanager.com/gtag/js?id=UA-141909368-1"></script>
+        <script type="text/plain" data-cookieconsent="statistics">
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-141909368-1');
+        </script>
+
         <!-- =======================================================
         Theme Name: DevFolio
         Theme URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
