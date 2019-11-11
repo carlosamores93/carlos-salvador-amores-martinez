5a54065b008af258c46b269f51c25762d3231611
Carlos <amorescarlos93@hotmail.com>
2019-11-11 14:28:20 +0100

    Closure and fix

---
+++ app/Console/Commands/TestCarlosAmores.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestCarlosAmores extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test Carlos Amores';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+        $this->info('******* CLOSURE ********');
+        $name = 'Carlos Amores';
+        // Closure: funciones anónimas
+        $greet = function() use($name){
+            $carlos = "Amores";
+            echo 'Hi ' . $name . ' ' ;
+            return ', bienvenido';
+        };
+        var_dump($greet);
+        //dd($greet);
+        //dd($greet());
+
+
+
+
+        $input = array(1, 2, 3, 4, 5, 6);
+        // Creates a new anonymous function and assigns it to a variable
+        $filter_even = function($item) {
+            return ($item % 2) == 0;
+        };
+        // Built-in array_filter accepts both the data and the function
+        $output = array_filter($input, $filter_even);
+        // The function doesn't need to be assigned to a variable. This is valid too:
+        /*$output = array_filter($input, function($item) {
+            return ($item % 2) == 0;
+        });*/
+        print_r($output);
+        //dd($filter_even());
+        //dd($filter_even);
+
+    }
+}
