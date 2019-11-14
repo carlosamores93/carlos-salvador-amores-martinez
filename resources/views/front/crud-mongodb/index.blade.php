9ed95b3651772f34979914241d4afac0d52d931d
Carlos <amorescarlos93@hotmail.com>
2019-11-14 12:11:58 +0100

    Navbar MongoDB and Vuejs Develop

--- resources/views/front/crud-mongodb/index.blade.php
+++ resources/views/front/crud-mongodb/index.blade.php
@@ -7,10 +7,33 @@
         <title>CRUD with MongoDB</title>
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link href="{{asset('css/app.css')}}" rel="stylesheet"> <!--Añadimos el css generado con webpack-->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet"> <!--Añadimos el css generado con webpack-->
     </head>
     <body>
         <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                      <a class="navbar-brand" href="{{ route('home') }}">HomePage</a>
+                      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{ route('crud-mongodb') }}">MongoDB</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="{{ route('crud-vuejs') }}">VueJs</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">AngularJs</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </nav>
+                </div>
+            </div>
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
--- resources/views/front/crud-vuejs/index.blade.php
+++ resources/views/front/crud-vuejs/index.blade.php
@@ -12,6 +12,29 @@
     <body>
             <div id="app" class="content"><!--La equita id debe ser app, como hemos visto en app.js-->
                 {{-- <example-component></example-component> --}}
+                <div class="row justify-content-center">
+                    <div class="col-md-8">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                          <a class="navbar-brand" href="{{ route('home') }}">HomePage</a>
+                          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                          </button>
+                          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                            <ul class="navbar-nav">
+                              <li class="nav-item">
+                                <a class="nav-link" href="{{ route('crud-mongodb') }}">MongoDB</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="{{ route('crud-vuejs') }}">VueJs</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">AngularJs</a>
+                              </li>
+                            </ul>
+                          </div>
+                        </nav>
+                    </div>
+                </div>
 
                 <article-component></article-component>
                 <!--Añadimos nuestro componente vuejs-->
