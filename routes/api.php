03424aa618b63e9edc8f171043a2372dada81f01
Carlos <amorescarlos93@hotmail.com>
2020-01-03 18:11:04 +0100

    API: Creamos las rutas

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,32 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::resource(
+    'buyers',
+    'Buyer\BuyerController',
+    ['only' => ['index', 'show']]
+);
+Route::resource(
+    'categories',
+    'Category\CategoryController',
+    ['except' => ['create', 'edit']]
+);
+Route::resource(
+    'transactions',
+    'Transaction\TransactionController',
+    ['only' => ['index', 'show']]
+);
+Route::resource(
+    'sellers',
+    'Seller\SellerController',
+    ['only' => ['index', 'show']]
+);
+Route::resource(
+    'users',
+    'User\UserController',
+    ['except' => ['create', 'edit']]
+);
+
 
 
 // List articles
